9fd7ee1e8421483e87db6e7ba9a4627fdf2f7bb2
Anders R. Clausen <anders.clausen@gu.se>
2022-09-12 12:03:12 +0200

    Update remove_cut_sites.py

--- remove_cut_sites.py
+++ remove_cut_sites.py
@@ -1,46 +1,28 @@
-
-# coding: utf-8
-
-
-
 import os
 import pandas as pd
-import csv
 
-print (os.getcwd())
-
-bed_file="KCl-uniq.human.SRR5035909_forward.bedgraph"
-cut_file="forward_hincII_human_new.txt"
-bed_df=pd.read_csv(bed_file,sep="\t",header=None,skiprows=1,nrows=5)
-cut_df=pd.read_csv("../"+cut_file,sep="\t",header=None,skiprows=1,nrows=4)
-
-def make_mask(file):
-    output=open("mask"+file,"w+")
-    with open("../"+file) as csv_file:
-        data=csv.reader(csv_file,delimiter="\t")
-        for row in data:
-            for i in range(-5,6):
-                chrm=row[0]
-                pos=int(row[1])+i
-                #line=chrm+","+str(pos)
-                #line=",".join(line)
-                line=row[0]+","+str(pos)
-                #print (row[0],pos)
-                #print(line)
-                output.write(line)
-    output.close()
-                
-make_mask(cut_file)
-
-cut_file.head()
+if not os.path.exists('./masked'):
+    os.makedirs('./masked')
 
 
-new_file=pd.merge(bed_file,cut_file)
+def mask_file():
+    files=[i for i in os.listdir("./bg/test_bg") if i.endswith("bedgraph")]
+    #files=["KCl-uniq.human.SRR5035928_forward.bedgraph"]
+    cut_file="./human_hincii_forward.txt"
+    print (files)
+    print ("Cut sites-file",cut_file)
+    path="./bg/test_bg/"
+    for file in files:
+        print ("Removing cut sites from",file)
+        bed_df=pd.read_csv(path+file,sep="\t",skiprows=1,header=None)
+        cut_df=pd.read_csv(cut_file,sep="\t",header=None)
+        cut_df[1]=cut_df[1]-5
+        merged_df=bed_df.copy()
+        for i in range(10):
+            cut_df[1]=cut_df[1]+1
+            merged_df = merged_df.merge(cut_df,indicator='i', how='outer').query('i == "left_only"').drop('i', axis=1)
+        print ("I have removed",bed_df.shape[0]-merged_df.shape[0],"sites")
+        print ("I have removed",bed_df[3].sum()-merged_df[3].sum(),"reads")
+        bed_df.to_csv("./masked/cut_removed"+file,index=None)
 
-mask=pd.read_csv("maskforward_hincII_human_new.txt",sep="\t")
-
-no_cut = bed_df.merge(mask.drop_duplicates(), on=[0,1], 
-                   how='left', indicator=True)
-
-print (no_cut.shape)
-
+mask_file()
